698313bd0c57c6fc15d9106e951235cbfc23bcfc
Mikegit1988 <mikenerly@gmail.com>
2022-08-06 23:15:10 +0000

    Add root chatroom controller and sessions controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #set up root page and login pages
+  root 'chatroom#index'
+  get 'login', to: 'sessions#new'
+  
 end
